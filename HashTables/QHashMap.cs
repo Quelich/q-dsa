1f3fc5d8ffb04bc35470526455d2d4ddda2e61ff
Emre Kılıç <emre1kilic@outlook.com>
2022-02-13 01:04:22 +0300

    add hashmap

--- HashTables/QHashMap.cs
+++ HashTables/QHashMap.cs
@@ -1,7 +1,6 @@
 namespace DataStructures
 {
-
-    class QHashMap<T1, T2>
+    public class QHashMap<T1, T2>
     {
         private T1[] keys;
         private T2[] values;
@@ -9,74 +8,135 @@ namespace DataStructures
         private int size;
 
         private int count;
-        QHashTable(int size)
+        public QHashMap(int size)
         {
             this.size = size;
-            hashTable = new int[size];
+            keys = new T1[size];
+            values = new T2[size];
             isOccupied = new bool[size];
             count = 0;
         }
 
-        void Insert(T1 key, T2 value)
+        public int Insert(T1 key, T2 value)
         {
-
-            // increment count
+            var isFree = false;
+            while (!isFree)
+            {
+                var hash = GetHashCode();
+                if (!isOccupied[hash])
+                {
+                    isFree = true;
+                    keys[hash] = key;
+                    values[hash] = value;
+                    isOccupied[hash] = true;
+                    count++;
+                    return 0;
+                }
+            }
+            Console.Error.Write("Something went wrong :/");
+            return -1;
         }
 
-        void Remove(T2 value)
+        public int Remove(T1 key)
         {
-            // decrement count
+            var idx = Search(key);
+            if (idx != -1)
+            {
+                keys[idx] = default(T1)!;
+                values[idx] = default(T2)!;
+                isOccupied[idx] = false;
+                count--;
+                return 0;
+            }
+            Console.Error.Write("Could not find key");
+            return -1;
         }
 
-        void Read(T1 key)
+        public string ReadByKey(T1 key)
         {
-
+            var idx = Search(key);
+            if (idx != -1)
+            {   
+                return values[idx] == null ? "null" : values[idx].ToString();
+            }
+            Console.Error.Write("Could not find key");
+            return "";
         }
 
-        void Update(T item, int idx)
+        public string ReadAll()
         {
-
+            var result = string.Empty;
+            for (var i = 0; i < size; i++)
+            {
+                if (isOccupied[i])
+                {
+                    result += $"{keys[i]} -> {values[i]}\n";
+                }
+            }
+            return result;
         }
 
+        public int Update(T1 key, T2 Value)
+        {
+            var idx = Search(key);
+            if (idx != -1)
+            {
+                values[idx] = Value;
+                return 0;
+            }
+            Console.Error.Write("Could not find key");
+            return -1;
+        }
+
+        //O(n)
+        //todo - make it O(lgn)
+        private int Search(T1 key)
+        {
+            for (var i = 0; i < keys.Length; i++)
+            {
+                if (keys[i]!.Equals(key))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
         // Find nearest prime number
         // key % nearest prime number
-        void HashCode()
+        private new int GetHashCode()
         {
-
-        }
-
-        void Print()
-        {
-
+            var prime = GetFloorPrime();
+            var num = GetRandomNumber();
+            return prime % num;
         }
 
         //Helper functions
-        private int FindNearestPrime()
+        private int GetRandomNumber()
+        {
+            return new Random().Next(1, size);
+        }
+        private int GetFloorPrime()
         {
             if (size == 1 || size == 2)
                 return 2;
             var isPrime = false;
-            var num = size;
+            var nearestPrime = size;
+            var result = 0;
             while (!isPrime)
             {
-                int nearestPrime;
-                for (var i = 2; i < num / 2; ++i)
+                for (var i = 2; i < nearestPrime / 2; i++)
                 {
-                    if (num % i == 0)
+                    if (nearestPrime % i == 0)
                     {
                         isPrime = false;
                         break;
                     }
-                    else
-                    {
-                        isPrime = true;
-                        nearestPrime = num;
-                        return nearestPrime;
-                    }
+                    isPrime = true;
+                    result = nearestPrime;
                 }
-                num++;
+                nearestPrime++;
             }
-            return 0;
+            return result;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var map = new QHashMap<int, string>(10);
+            var result = string.Empty;
+            map.Insert(1, "Emre");
+            map.Insert(2, "Ali");
+            map.Insert(3, "Veli");
+            result = map.ReadAll();
+            System.Console.WriteLine(result);
+            map.Remove(3);
+            result = map.ReadAll();
+            System.Console.WriteLine(result);
+            map.Update(1, "Emre Kılıç");
+            result = map.ReadAll();
+            System.Console.WriteLine(result);
+
+        }
+    }
+}
